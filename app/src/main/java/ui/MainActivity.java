dd019f0146651509682fac96ff5af5d9b0ba4bea
icextreme <74882450+icextreme@users.noreply.github.com>
2020-02-28 22:39:08 -0800

    Migrate app to AndroidX and update Gradle build
    Migrate app to AndroidX. Update Gradle build from 3.3.0 to 3.6.1. Add OpenCSV library for CSV parsing. Edit ExampleInstrumentalTest to reflect latest version. Add model/ui packages for separation. Add /docs folder and roles1.txt for explaining roles.

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package ca.cmpt276.restauranthealthinspection;
+package ui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ca.cmpt276.restauranthealthinspection.R;
 
 public class MainActivity extends AppCompatActivity {
 
