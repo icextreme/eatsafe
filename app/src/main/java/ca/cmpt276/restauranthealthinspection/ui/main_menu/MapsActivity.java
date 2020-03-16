3a0563c0c6dcb747668ea373a0c40e4463d5d86f
Alex-PC <mql@sfu.ca>
2020-03-15 17:00:58 -0700

    Map showing device location.

--- app/src/main/java/ca/cmpt276/restauranthealthinspection/ui/main_menu/MapsActivity.java
+++ app/src/main/java/ca/cmpt276/restauranthealthinspection/ui/main_menu/MapsActivity.java
@@ -14,18 +14,38 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import ca.cmpt276.restauranthealthinspection.R;
 
+import static android.telephony.CellLocation.requestLocationUpdate;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: map loaded");
+        Toast.makeText(this, "Eat Safe!", Toast.LENGTH_SHORT).show();
+        map = googleMap;
+
+        if (locationPermissionGranted) {
+            //setupDeviceLocationUpdate();
+            getDeviceCurrentLocation();
+            map.setMyLocationEnabled(true);
+        }
+    }
 
     private static final String TAG = "MapsActivity";
 
@@ -38,39 +58,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private boolean locationPermissionGranted;
     private FusedLocationProviderClient fusedLocationProviderClient;
 
+    private Location currentLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
         getLocationPermission();
+        createLocationRequest();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Log.d(TAG, "onMapReady: map loaded");
-        Toast.makeText(this, "Eat Safe!", Toast.LENGTH_SHORT).show();
-        map = googleMap;
+    //Reference: https://developer.android.com/training/location/change-location-settings
+    protected void createLocationRequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        if(locationPermissionGranted){
-            getDeviceCurrentLocation();
-            map.setMyLocationEnabled(true);
-        }
-        // Add a marker in Sydney and move the camera
-/*        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
-
-
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
     }
 
     private void getDeviceCurrentLocation() {
@@ -80,17 +87,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         try {
             Task location = fusedLocationProviderClient.getLastLocation();
-
             location.addOnCompleteListener(new OnCompleteListener() {
                 @Override
                 public void onComplete(@NonNull Task task) {
                     if (task.isSuccessful()) {
                         Log.d(TAG, "getDeviceCurrentLocation: got current location");
-
-                        //note: getLastLocation return the last location stored. If no location stored -> null ref.
-                        //if getting a null reference, open google map and retrieve a location first.
-                        //this will be updated to continuously update gps.
-                        Location currentLocation = (Location) task.getResult();
+                        currentLocation = (Location) task.getResult();
                         moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
 
                     } else {
