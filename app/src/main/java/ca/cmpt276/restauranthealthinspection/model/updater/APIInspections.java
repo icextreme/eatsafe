edcfa02d07f3cc0716dc484d27ec584a78a3f186
icextreme <74882450+icextreme@users.noreply.github.com>
2020-03-19 14:21:25 -0700

    Add updater files
    Add updater files

---
+++ app/src/main/java/ca/cmpt276/restauranthealthinspection/model/updater/APIInspections.java
@@ -0,0 +1,9 @@
+package ca.cmpt276.restauranthealthinspection.model.updater;
+
+import java.util.Calendar;
+
+public class APIInspections {
+    private Calendar lastModified;
+    private String url;
+    
+}
---
+++ app/src/main/java/ca/cmpt276/restauranthealthinspection/model/updater/APIRestaurants.java
@@ -0,0 +1,9 @@
+package ca.cmpt276.restauranthealthinspection.model.updater;
+
+import java.util.Calendar;
+
+public class APIRestaurants {
+
+    private Calendar lastModified;
+    private String url;
+}
